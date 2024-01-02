0fce219ef136f062fc77d3a6c2e789d3ad8be975
flavioepimentel <flavio.ericksan@hotmail.com>
2024-01-02 07:50:43 -0300

    feat: :truck: Adiciona novas rotas para report e review

--- app/routes/api.php
+++ app/routes/api.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Http\Controllers\AgendaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientsController;
-use App\Http\Controllers\ServicesController;
+use App\Http\Controllers\ReviewsController;
+use App\Http\Controllers\ReportController;
 use App\Http\Controllers\VehiclesController;
 
 /*
@@ -27,20 +27,20 @@ Route::delete('/clients/{id}', [ClientsController::class, 'destroy']);
 Route::get('/vehicles', [VehiclesController::class, 'index']);
 Route::get('/vehicles/{id}', [VehiclesController::class, 'show']);
 Route::put('/vehicles/{id}', [VehiclesController::class, 'update']);
-Route::post('/vehicles', [VehiclesController::class, 'store']);
+Route::post('/vehicles/create', [VehiclesController::class, 'store']);
 Route::delete('/vehicles/{id}', [VehiclesController::class, 'destroy']);
 
-Route::get('/services', [ServicesController::class, 'index']);
-Route::get('/services/{id}', [ServicesController::class, 'show']);
-Route::put('/services/{id}', [ServicesController::class, 'update']);
-Route::post('/services', [ServicesController::class, 'store']);
-Route::delete('/services/{id}', [ServicesController::class, 'destroy']);
+Route::get('/review', [ReviewsController::class, 'index']);
+Route::get('/review/{id}', [ReviewsController::class, 'show']);
+Route::put('/review/{id}', [ReviewsController::class, 'update']);
+Route::post('/review/create', [ReviewsController::class, 'store']);
+Route::delete('/review/{id}', [ReviewsController::class, 'destroy']);
 
-Route::get('/agenda', [AgendaController::class, 'index']);
-Route::get('/agenda/{id}', [AgendaController::class, 'show']);
-Route::put('/agenda/{id}', [AgendaController::class, 'update']);
-Route::post('/agenda', [AgendaController::class, 'store']);
-Route::delete('/agenda/{id}', [AgendaController::class, 'destroy']);
+Route::get('/report/vehiclePerPerson', [ReportController::class, 'index']);
+Route::get('/report/vehiclePerGender', [ReportController::class, 'show']);
+Route::get('/report/vehiclePerBrand', [ReportController::class, 'update']);
+Route::get('/report/vehiclePerBrandGender', [ReportController::class, 'destroy']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
