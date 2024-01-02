1547402bd6f4d54fefcdd1a393e99713cac5dd55
flavioepimentel <flavio.ericksan@hotmail.com>
2024-01-02 14:43:40 -0300

    fix: :truck: Adiona novas rotas para relatórios

--- app/routes/api.php
+++ app/routes/api.php
@@ -4,7 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientsController;
 use App\Http\Controllers\ReviewsController;
-use App\Http\Controllers\ReportController;
+use App\Http\Controllers\VehiclesReportController;
+use App\Http\Controllers\ClientsReportController;
+use App\Http\Controllers\ReviewsReportController;
 use App\Http\Controllers\VehiclesController;
 
 /*
@@ -36,10 +38,56 @@ Route::put('/review/{id}', [ReviewsController::class, 'update']);
 Route::post('/review/create', [ReviewsController::class, 'store']);
 Route::delete('/review/{id}', [ReviewsController::class, 'destroy']);
 
-Route::get('/report/vehiclePerPerson', [ReportController::class, 'index']);
-Route::get('/report/vehiclePerGender', [ReportController::class, 'show']);
-Route::get('/report/vehiclePerBrand', [ReportController::class, 'update']);
-Route::get('/report/vehiclePerBrandGender', [ReportController::class, 'destroy']);
+Route::get(
+    '/report/vehicleByPerson',
+    [VehiclesReportController::class, 'vehicleByPerson']
+);
+Route::get(
+    '/report/vehicleByGender',
+    [VehiclesReportController::class, 'vehicleByGender']
+);
+Route::get(
+    '/report/vehicleByBrand',
+    [VehiclesReportController::class, 'vehicleByBrand']
+);
+Route::get(
+    '/report/vehicleByBrandGender',
+    [VehiclesReportController::class, 'vehicleByBrandGender']
+);
+
+Route::get(
+    '/report/averageAgeByGender',
+    [ClientsReportController::class, 'averageAgeByGender']
+);
+
+Route::get(
+    '/report/reviewsByBrand',
+    [ReviewsReportController::class, 'reviewsByBrand']
+);
+
+Route::get(
+    '/report/reviewsByClient',
+    [ReviewsReportController::class, 'reviewsByClient']
+);
+
+Route::get(
+    '/report/averageTimeBetweenReviews/{id}',
+    [ReviewsReportController::class, 'averageTimeBetweenReviews']
+);
+
+Route::get(
+    '/report/reviewsByPeriod/{beginFilter}/{endFilter}',
+    [ReviewsReportController::class, 'reviewsByPeriod']
+);
+
+Route::get(
+    '/report/predictionReviews/',
+    [ReviewsReportController::class, 'predictionReviews']
+);
+Route::get(
+    '/report/predictionReviewsV2/',
+    [ReviewsReportController::class, 'predictionReviewsV2']
+);
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
